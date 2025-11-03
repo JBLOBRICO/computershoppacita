f46f891d1be0523873931bfe899cb638cfca3c36
JBLOBRICO <c2903-23@itmlyceumalabang.onmicrosoft.com>
2025-11-03 21:05:47 +0800

    sdadsa

--- sessionfrm.Designer.vb
+++ sessionfrm.Designer.vb
@@ -2,18 +2,6 @@
 Partial Class sessionfrm
     Inherits System.Windows.Forms.Form
 
-    ' Clean up resources
-    <System.Diagnostics.DebuggerNonUserCode()>
-    Protected Overrides Sub Dispose(disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
-
     Private components As System.ComponentModel.IContainer
 
     <System.Diagnostics.DebuggerStepThrough()>
@@ -28,6 +16,8 @@ Partial Class sessionfrm
         Me.lblTimer = New System.Windows.Forms.Label()
         Me.lblSales = New System.Windows.Forms.Label()
         Me.txtSales = New System.Windows.Forms.TextBox()
+        Me.lblSessionType = New System.Windows.Forms.Label()
+        Me.cmbSessionType = New System.Windows.Forms.ComboBox()
         Me.btnStart = New System.Windows.Forms.Button()
         Me.btnEnd = New System.Windows.Forms.Button()
         Me.sessionTimer = New System.Windows.Forms.Timer(Me.components)
@@ -37,38 +27,25 @@ Partial Class sessionfrm
         '
         'pnlHeader
         '
-        Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(58, Byte), Integer), CType(CType(90, Byte), Integer), CType(CType(164, Byte), Integer))
+        Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(58, 90, 164)
         Me.pnlHeader.Controls.Add(Me.lblTitle)
         Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
-        Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
-        Me.pnlHeader.Margin = New System.Windows.Forms.Padding(2)
-        Me.pnlHeader.Name = "pnlHeader"
         Me.pnlHeader.Size = New System.Drawing.Size(956, 39)
-        Me.pnlHeader.TabIndex = 2
         '
         'lblTitle
         '
         Me.lblTitle.AutoSize = True
         Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Bold)
         Me.lblTitle.ForeColor = System.Drawing.Color.White
-        Me.lblTitle.Location = New System.Drawing.Point(15, 10)
-        Me.lblTitle.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblTitle.Name = "lblTitle"
-        Me.lblTitle.Size = New System.Drawing.Size(226, 30)
-        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Location = New System.Drawing.Point(15, 5)
         Me.lblTitle.Text = "💻 Manage Sessions"
         '
         'pnlComputers
         '
         Me.pnlComputers.AutoScroll = True
-        Me.pnlComputers.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.pnlComputers.BackColor = System.Drawing.Color.FromArgb(240, 242, 245)
         Me.pnlComputers.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.pnlComputers.Location = New System.Drawing.Point(0, 39)
-        Me.pnlComputers.Margin = New System.Windows.Forms.Padding(2)
-        Me.pnlComputers.Name = "pnlComputers"
         Me.pnlComputers.Padding = New System.Windows.Forms.Padding(15, 13, 15, 13)
-        Me.pnlComputers.Size = New System.Drawing.Size(731, 359)
-        Me.pnlComputers.TabIndex = 0
         '
         'pnlInfo
         '
@@ -76,98 +53,85 @@ Partial Class sessionfrm
         Me.pnlInfo.Controls.Add(Me.lblPCName)
         Me.pnlInfo.Controls.Add(Me.lblStatus)
         Me.pnlInfo.Controls.Add(Me.lblTimer)
+        Me.pnlInfo.Controls.Add(Me.lblSessionType)
+        Me.pnlInfo.Controls.Add(Me.cmbSessionType)
         Me.pnlInfo.Controls.Add(Me.lblSales)
         Me.pnlInfo.Controls.Add(Me.txtSales)
         Me.pnlInfo.Controls.Add(Me.btnStart)
         Me.pnlInfo.Controls.Add(Me.btnEnd)
         Me.pnlInfo.Dock = System.Windows.Forms.DockStyle.Right
-        Me.pnlInfo.Location = New System.Drawing.Point(731, 39)
-        Me.pnlInfo.Margin = New System.Windows.Forms.Padding(2)
-        Me.pnlInfo.Name = "pnlInfo"
         Me.pnlInfo.Padding = New System.Windows.Forms.Padding(15, 13, 15, 13)
         Me.pnlInfo.Size = New System.Drawing.Size(225, 359)
-        Me.pnlInfo.TabIndex = 1
         '
         'lblPCName
         '
         Me.lblPCName.AutoSize = True
         Me.lblPCName.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold)
-        Me.lblPCName.Location = New System.Drawing.Point(8, 13)
-        Me.lblPCName.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblPCName.Name = "lblPCName"
-        Me.lblPCName.Size = New System.Drawing.Size(111, 25)
-        Me.lblPCName.TabIndex = 0
         Me.lblPCName.Text = "PC Name: -"
+        Me.lblPCName.Location = New System.Drawing.Point(8, 10)
         '
         'lblStatus
         '
         Me.lblStatus.AutoSize = True
         Me.lblStatus.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.lblStatus.Location = New System.Drawing.Point(8, 39)
-        Me.lblStatus.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblStatus.Name = "lblStatus"
-        Me.lblStatus.Size = New System.Drawing.Size(65, 21)
-        Me.lblStatus.TabIndex = 1
         Me.lblStatus.Text = "Status: -"
+        Me.lblStatus.Location = New System.Drawing.Point(8, 40)
         '
         'lblTimer
         '
         Me.lblTimer.AutoSize = True
         Me.lblTimer.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.lblTimer.Location = New System.Drawing.Point(8, 65)
-        Me.lblTimer.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblTimer.Name = "lblTimer"
-        Me.lblTimer.Size = New System.Drawing.Size(137, 21)
-        Me.lblTimer.TabIndex = 2
         Me.lblTimer.Text = "Usage Time: 00:00"
+        Me.lblTimer.Location = New System.Drawing.Point(8, 70)
+        '
+        'lblSessionType
+        '
+        Me.lblSessionType.AutoSize = True
+        Me.lblSessionType.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblSessionType.Text = "Session Type:"
+        Me.lblSessionType.Location = New System.Drawing.Point(8, 100)
+        '
+        'cmbSessionType
+        '
+        Me.cmbSessionType.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.cmbSessionType.Items.AddRange(New Object() {"Open", "Fixed"})
+        Me.cmbSessionType.DropDownStyle = ComboBoxStyle.DropDownList
+        Me.cmbSessionType.Location = New System.Drawing.Point(8, 125)
+        Me.cmbSessionType.Size = New System.Drawing.Size(188, 29)
+        Me.cmbSessionType.SelectedIndex = 0
         '
         'lblSales
         '
         Me.lblSales.AutoSize = True
         Me.lblSales.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.lblSales.Location = New System.Drawing.Point(8, 91)
-        Me.lblSales.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.lblSales.Name = "lblSales"
-        Me.lblSales.Size = New System.Drawing.Size(72, 21)
-        Me.lblSales.TabIndex = 3
-        Me.lblSales.Text = "Sales (₱):"
+        Me.lblSales.Text = "Amount (₱):"
+        Me.lblSales.Location = New System.Drawing.Point(8, 160)
         '
         'txtSales
         '
         Me.txtSales.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.txtSales.Location = New System.Drawing.Point(8, 110)
-        Me.txtSales.Margin = New System.Windows.Forms.Padding(2)
-        Me.txtSales.Name = "txtSales"
+        Me.txtSales.Location = New System.Drawing.Point(8, 185)
         Me.txtSales.Size = New System.Drawing.Size(188, 29)
-        Me.txtSales.TabIndex = 4
         '
         'btnStart
         '
-        Me.btnStart.BackColor = System.Drawing.Color.FromArgb(CType(CType(58, Byte), Integer), CType(CType(90, Byte), Integer), CType(CType(164, Byte), Integer))
-        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStart.BackColor = System.Drawing.Color.FromArgb(58, 90, 164)
+        Me.btnStart.FlatStyle = FlatStyle.Flat
         Me.btnStart.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Bold)
-        Me.btnStart.ForeColor = System.Drawing.Color.White
-        Me.btnStart.Location = New System.Drawing.Point(8, 150)
-        Me.btnStart.Margin = New System.Windows.Forms.Padding(2)
-        Me.btnStart.Name = "btnStart"
+        Me.btnStart.ForeColor = Color.White
+        Me.btnStart.Location = New System.Drawing.Point(8, 225)
         Me.btnStart.Size = New System.Drawing.Size(188, 26)
-        Me.btnStart.TabIndex = 5
         Me.btnStart.Text = "▶ Start Session"
-        Me.btnStart.UseVisualStyleBackColor = False
         '
         'btnEnd
         '
-        Me.btnEnd.BackColor = System.Drawing.Color.FromArgb(CType(CType(200, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer))
-        Me.btnEnd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnEnd.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Bold)
-        Me.btnEnd.ForeColor = System.Drawing.Color.White
-        Me.btnEnd.Location = New System.Drawing.Point(8, 182)
-        Me.btnEnd.Margin = New System.Windows.Forms.Padding(2)
-        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.BackColor = Color.FromArgb(200, 60, 70)
+        Me.btnEnd.FlatStyle = FlatStyle.Flat
+        Me.btnEnd.Font = New System.Drawing.Font("Segoe UI", 11.0!, FontStyle.Bold)
+        Me.btnEnd.ForeColor = Color.White
+        Me.btnEnd.Location = New System.Drawing.Point(8, 260)
         Me.btnEnd.Size = New System.Drawing.Size(188, 26)
-        Me.btnEnd.TabIndex = 6
         Me.btnEnd.Text = "⏹ End Session"
-        Me.btnEnd.UseVisualStyleBackColor = False
         '
         'sessionTimer
         '
@@ -175,23 +139,19 @@ Partial Class sessionfrm
         '
         'sessionfrm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleMode = AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(956, 398)
         Me.Controls.Add(Me.pnlComputers)
         Me.Controls.Add(Me.pnlInfo)
         Me.Controls.Add(Me.pnlHeader)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Margin = New System.Windows.Forms.Padding(2)
-        Me.Name = "sessionfrm"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.StartPosition = FormStartPosition.CenterScreen
         Me.Text = "Manage Sessions"
         Me.pnlHeader.ResumeLayout(False)
         Me.pnlHeader.PerformLayout()
         Me.pnlInfo.ResumeLayout(False)
         Me.pnlInfo.PerformLayout()
         Me.ResumeLayout(False)
-
     End Sub
 
     Friend WithEvents pnlHeader As Panel
@@ -206,4 +166,6 @@ Partial Class sessionfrm
     Friend WithEvents btnStart As Button
     Friend WithEvents btnEnd As Button
     Friend WithEvents sessionTimer As Timer
+    Friend WithEvents lblSessionType As Label
+    Friend WithEvents cmbSessionType As ComboBox
 End Class
--- sessionfrm.vb
+++ sessionfrm.vb
@@ -1,23 +1,26 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class sessionfrm
-    ' ====== CONFIGURABLE SETTINGS ======
+    ' ====== CONFIG ======
     Private ratePerHour As Decimal = 20D
-    Private sessionDuration As TimeSpan = TimeSpan.FromMinutes(60) ' Default 1 hour session
-
-    ' ====== TRACKING VARIABLES ======
-    Private PCStatus As New Dictionary(Of Integer, Boolean)
-    Private StartTimes As New Dictionary(Of Integer, DateTime)
+    Private sessionDuration As TimeSpan
+    Private PCStatus As New Dictionary(Of Integer, String)
     Private SelectedPCID As Integer = -1
     Private SelectedPCName As String = ""
     Private currentStartTime As DateTime
     Private timerRunning As Boolean = False
     Private notificationShown As Boolean = False
 
-    ' ====== FORM LOAD ======
+    ' ====== LOAD FORM ======
     Private Sub sessionfrm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadComputers()
         lblTimer.Text = "Usage Time: 00:00"
+        txtSales.Enabled = False
+        AddHandler cmbSessionType.SelectedIndexChanged, AddressOf cmbSessionType_SelectedIndexChanged
+    End Sub
+
+    Private Sub cmbSessionType_SelectedIndexChanged(sender As Object, e As EventArgs)
+        txtSales.Enabled = (cmbSessionType.SelectedItem.ToString() = "Fixed")
     End Sub
 
     ' ====== LOAD COMPUTERS ======
@@ -57,10 +60,9 @@ Public Class sessionfrm
                 AddHandler btnPC.Click, AddressOf PC_Click
                 pnlComputers.Controls.Add(btnPC)
 
-                PCStatus(pcID) = (status = "In Use")
+                PCStatus(pcID) = status
             End While
             dr.Close()
-
         Catch ex As Exception
             MessageBox.Show("Error loading computers: " & ex.Message)
         Finally
@@ -68,29 +70,58 @@ Public Class sessionfrm
         End Try
     End Sub
 
-    ' ====== SELECT COMPUTER ======
+    ' ====== SELECT PC ======
     Private Sub PC_Click(sender As Object, e As EventArgs)
         Dim btn As Button = CType(sender, Button)
         SelectedPCID = btn.Tag
         SelectedPCName = btn.Text
         lblPCName.Text = "PC Name: " & SelectedPCName
+        lblStatus.Text = "Status: " & PCStatus(SelectedPCID)
     End Sub
 
     ' ====== START SESSION ======
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
         If SelectedPCID = -1 Then
-            MessageBox.Show("Please select a PC first.")
+            MessageBox.Show("Please select a PC first.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
         End If
 
-        If PCStatus(SelectedPCID) Then
-            MessageBox.Show("This PC is already in use.")
+        Dim status = PCStatus(SelectedPCID)
+        If status = "In Use" Then
+            MessageBox.Show("This PC is already in use.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
+        ElseIf status = "Maintenance" Then
+            MessageBox.Show("This PC is under maintenance.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Determine session type
+        Dim sessionType As String = cmbSessionType.SelectedItem.ToString()
+        Dim fixedAmount As Decimal = 0
+
+        If sessionType = "Fixed" Then
+            If String.IsNullOrWhiteSpace(txtSales.Text) Then
+                MessageBox.Show("Please enter a fixed amount.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+            If Not Decimal.TryParse(txtSales.Text, fixedAmount) Then
+                MessageBox.Show("Enter a valid numeric amount.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+            If fixedAmount <= 0 Then
+                MessageBox.Show("Amount must be greater than 0.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+
+            sessionDuration = TimeSpan.FromMinutes((fixedAmount / ratePerHour) * 60) ' convert hours to minutes
+        Else
+            sessionDuration = TimeSpan.Zero ' open session
         End If
 
         Try
             OpenConnection()
 
+            ' Insert sale
             Dim insertQuery As String = "
                 INSERT INTO sales (ComputerID, UserID, StartTime, RatePerHour, PaymentStatus)
                 VALUES (@compID, @userID, NOW(), @rate, 'Unpaid');"
@@ -100,17 +131,18 @@ Public Class sessionfrm
             cmd.Parameters.AddWithValue("@rate", ratePerHour)
             cmd.ExecuteNonQuery()
 
+            ' Update PC status
             Dim updateQuery As String = "UPDATE computers SET Status='In Use' WHERE ComputerID=@id"
             cmd = New MySqlCommand(updateQuery, conn)
             cmd.Parameters.AddWithValue("@id", SelectedPCID)
             cmd.ExecuteNonQuery()
 
-            ' ✅ Start timer
+            ' Start timer
             currentStartTime = DateTime.Now
             timerRunning = True
             sessionTimer.Start()
-            lblTimer.Text = "Usage Time: 00:00"
             notificationShown = False
+            lblTimer.Text = "Usage Time: 00:00"
 
             MessageBox.Show("Session started for " & SelectedPCName)
             LoadComputers()
@@ -125,13 +157,12 @@ Public Class sessionfrm
     ' ====== END SESSION ======
     Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
         If SelectedPCID = -1 Then
-            MessageBox.Show("Please select a PC first.")
+            MessageBox.Show("Please select a PC first.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
         End If
 
         Try
             OpenConnection()
-
             Dim getSaleQuery As String = "SELECT SaleID, StartTime FROM sales WHERE ComputerID=@id AND PaymentStatus='Unpaid' ORDER BY SaleID DESC LIMIT 1"
             cmd = New MySqlCommand(getSaleQuery, conn)
             cmd.Parameters.AddWithValue("@id", SelectedPCID)
@@ -146,27 +177,29 @@ Public Class sessionfrm
                 Dim hours As Decimal = CDec(duration.TotalHours)
                 Dim totalAmount As Decimal = Math.Round(hours * ratePerHour, 2)
 
+                ' Update sale
                 Dim updateSale As String = "UPDATE sales SET EndTime=NOW(), TotalAmount=@total, PaymentStatus='Paid' WHERE SaleID=@saleID"
                 cmd = New MySqlCommand(updateSale, conn)
                 cmd.Parameters.AddWithValue("@total", totalAmount)
                 cmd.Parameters.AddWithValue("@saleID", saleID)
                 cmd.ExecuteNonQuery()
 
+                ' Reset PC
                 Dim updatePC As String = "UPDATE computers SET Status='Available' WHERE ComputerID=@id"
                 cmd = New MySqlCommand(updatePC, conn)
                 cmd.Parameters.AddWithValue("@id", SelectedPCID)
                 cmd.ExecuteNonQuery()
 
-                sessionTimer.Stop()
                 timerRunning = False
+                sessionTimer.Stop()
                 lblTimer.Text = "Usage Time: 00:00"
 
-                MessageBox.Show($"{SelectedPCName} session ended. Total: ₱{totalAmount}", "Session Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Session ended. Total Amount: ₱" & totalAmount.ToString("F2"))
                 LoadComputers()
             Else
-                MessageBox.Show("No active session found for this PC.")
+                dr.Close()
+                MessageBox.Show("No active session found for this PC.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             End If
-
         Catch ex As Exception
             MessageBox.Show("Error ending session: " & ex.Message)
         Finally
@@ -180,21 +213,19 @@ Public Class sessionfrm
             Dim elapsed As TimeSpan = DateTime.Now - currentStartTime
             lblTimer.Text = "Usage Time: " & elapsed.ToString("hh\:mm\:ss")
 
-            ' ⏰ Show 5-minute warning
-            Dim remaining As TimeSpan = sessionDuration - elapsed
-            If remaining.TotalMinutes <= 5 AndAlso Not notificationShown Then
-                MessageBox.Show("⚠️ Session will end in 5 minutes. Please save your work.", "Reminder", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                notificationShown = True
+            ' Show 5-min remaining notification for fixed session
+            If sessionDuration.TotalMinutes > 0 AndAlso Not notificationShown Then
+                Dim remaining As TimeSpan = sessionDuration - elapsed
+                If remaining.TotalMinutes <= 5 AndAlso remaining.TotalMinutes > 0 Then
+                    MessageBox.Show("⏰ 5 minutes left on this session!", "Reminder", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    notificationShown = True
+                End If
             End If
 
-            ' ⏰ Auto-stop when session time is up
-            If elapsed >= sessionDuration Then
-                sessionTimer.Stop()
-                timerRunning = False
-                lblTimer.Text = "Usage Time: 00:00"
-                MessageBox.Show("⏰ Session time is over!", "Session Ended", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ' Auto-end fixed session
+            If sessionDuration.TotalMinutes > 0 AndAlso elapsed >= sessionDuration Then
+                btnEnd.PerformClick()
             End If
         End If
     End Sub
-
 End Class
